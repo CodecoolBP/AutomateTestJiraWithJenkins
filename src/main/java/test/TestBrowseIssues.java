e588a3583eb34149784921fa7cd727198b9b07dc
Andras Fulop <andrasfulop86@gmail.com>
2019-03-21 12:06:03 +0100

    test enabled

--- src/main/java/test/TestBrowseIssues.java
+++ src/main/java/test/TestBrowseIssues.java
@@ -77,7 +77,7 @@ public class TestBrowseIssues {
         Assertions.assertTrue(browseIssues.openIssuesOfTestProject("Jeti").isDisplayed(), "No such project available");
     }
 
-    @Disabled
+
     @DisplayName("Validate that Jeti project has 3 issues at least")
     @Test
     public void checkJetiProjectIssues(){
